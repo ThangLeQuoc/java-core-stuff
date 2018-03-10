98fef229b84781912597e3a6b76e0b77999ed976
Thang Le Quoc <silverhair.guy@gmail.com>
2018-03-10 20:44:49 +0700

    init commit

---
+++ src/com/thanglequoc/song/comparator/Song.java
@@ -0,0 +1,49 @@
+package com.thanglequoc.song.comparator;
+
+public class Song {
+    private String title;
+    private String album;
+    private String artist;
+    private double price;
+    
+    public Song() {}
+
+    public Song(String title, String album, String artist, double price) {
+	this.title = title;
+	this.album = album;
+	this.artist = artist;
+	this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
